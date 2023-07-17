df047693e42bb82d531f4cf3130908d011eca8d3
Ldsc2002 <luissantoscuellar@gmail.com>
2023-07-17 17:31:02 -0600

    Boilerplate

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from '../style/App.module.css'
+
+function App() {
+    return (
+        <div className={classes.appContainer}>
+            Hello World!
+        </div>
+    )
+}
+
+export default App
